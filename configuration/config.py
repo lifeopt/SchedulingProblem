63e75da4006f1e5d9757c084195ad9896edc0be2
lifeopt <81667929+lifeopt@users.noreply.github.com>
2023-04-13 17:10:44 +0900

    Update config.py

--- configuration/config.py
+++ configuration/config.py
@@ -1,8 +1,16 @@
 import json
+import os
 
-with open('configuration/config.json') as config_file:
+# 현재 스크립트 파일의 경로를 얻습니다.
+script_directory = os.path.dirname(os.path.abspath(__file__))
+
+# config.json 파일의 상대 경로를 구성합니다.
+config_file_path = os.path.join(script_directory, 'config.json')
+
+# config.json 파일을 읽습니다.
+with open(config_file_path) as config_file:
     config = json.load(config_file)
-
+    
 # test
 if __name__ == '__main__':
     learning_rate = float(config['learning_rate'])
