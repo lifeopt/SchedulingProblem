319f0cda141299a2c0f8d786cbf136eca601b155
lifeopt <81667929+lifeopt@users.noreply.github.com>
2023-04-19 17:39:53 +0900

    Update config.py

--- configuration/config.py
+++ configuration/config.py
@@ -1,26 +1,18 @@
-import json
+import yaml
 import os
+import sys
 
 # 현재 스크립트 파일의 경로를 얻습니다.
 script_directory = os.path.dirname(os.path.abspath(__file__))
 
 # config.json 파일의 상대 경로를 구성합니다.
-config_file_path = os.path.join(script_directory, 'config.json')
+config_file_path = os.path.join(script_directory, 'config.yaml')
 
-# config.json 파일을 읽습니다.
+# config.yaml 파일을 읽습니다.
 with open(config_file_path) as config_file:
-    config = json.load(config_file)
+    config = yaml.safe_load(config_file)
     
 # test
 if __name__ == '__main__':
-    learning_rate = float(config['learning_rate'])
-    batch_size = int(config['batch_size'])
-    epochs = int(config['epochs'])
-    input_size = int(config['input_size'])
-    hidden_size = int(config['hidden_size'])
-    output_size = int(config['output_size'])
-    
-    num_envs = int(config['num_envs'])
-    
-    
-    max_processing_time = int(config['max_processing_time'])
+    actor_lr = config["agent"]["actor_lr"]
+    critic_lr = config["agent"]["critic_lr"]
