e5ee1e10e285326a961ba90366b753b7af4dc7c2
lifeopt <81667929+lifeopt@users.noreply.github.com>
2023-04-19 17:43:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import numpy as np
 import data.data_load as data_load
 import gymnasium as gym
 import env.register
@@ -7,126 +6,77 @@ import env.gssp_env
 import torch
 from networks.A2C import A2C
 from configuration.config import config
-from tqdm import tqdm
-import utility
+from train import save_load_weights
+from train import train
+import sys
+from pathlib import Path
+from utils import plot_results
 
-input_file_path = r'C:\Users\JS\Desktop\코드\01_ORScheduler\MYJSSP\data\taillard\open_shop_scheduling\tai4_4.txt'
 
+script_dir = Path(__file__).resolve().parent
+sys.path.append(str(script_dir.parent / "configuration"))
+
+input_file_path = config['train']['input_file_path']
 import numpy as np
 
-# environment hyperparams
-n_envs = config['num_envs']
-n_updates = 1000
-n_steps_per_update = 128
-randomize_domain = False
+n_envs = config['envs']['num_envs']
+n_updates = config['envs']['num_updates']
+use_cuda = config['envs']['use_cuda']
+randomize_domain = config['envs']['randomize_domain']
+actor_lr = config['agent']['actor_lr']
+critic_lr = config['agent']['critic_lr']
+is_save_weights = config['model_utils']['save_weights']
+is_load_weights = config['model_utils']['load_weights']
+num_instance_limit = config['train']['num_instance_limit']
 
-# agent hyperparams
-gamma = 0.999
-lam = 0.95  # hyperparameter for GAE
-ent_coef = 0.01  # coefficient for the entropy bonus (to encourage exploration)
-actor_lr = 0.001
-critic_lr = 0.005
-   
 def main():  
     
     instances = data_load.read_input_data(input_file_path)
-
-    for _, instance in enumerate(instances):
+    for i, instance in enumerate(instances):
+        if num_instance_limit >= i:
+            continue
         # num_jobs, num_machines, processing_times, machines
         (num_jobs, num_machines, processing_times, machines, operations_data,
          due_date, num_features, converted_processing_times, max_T, num_actions) = instance
-        
         # Replace the default worker function with your custom worker function
         # gym.vector.async_vector_env._worker = custom_worker
         
-        
-        envs = gym.vector.AsyncVectorEnv(
-            [
-                lambda: gym.make('GSSP-v0', num_jobs = num_jobs, num_machines =  num_machines,
-                            operations_data = operations_data, due_date = due_date, max_T = max_T, max_episode_steps=600)
-                for i in range(n_envs)
-            ]
-        )
+        if randomize_domain:
+            envs = gym.vector.AsyncVectorEnv(
+                [
+                    lambda: gym.make('GSSP-v0', num_jobs = num_jobs, num_machines =  num_machines,
+                        operations_data = operations_data, due_date = due_date, max_T = max_T, max_episode_steps=600)
+                    for i in range(n_envs)
+                ]
+            )
+        else:
+            envs = gym.vector.make('GSSP-v0', num_jobs = num_jobs, num_machines =  num_machines,
+                operations_data = operations_data, due_date = due_date, max_T = max_T, num_envs = n_envs, max_episode_steps=600)
         
         # set the device
-        use_cuda = config['use_cuda']
         if use_cuda:
             device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         else:
             device = torch.device("cpu")
-        
        
         # init the agent
-        agent = A2C(num_features, num_actions, device, config['critic_lr'], config['actor_lr'], n_envs)
-        
+        agent = A2C(num_features, num_actions, device, critic_lr, actor_lr, n_envs)
         
         # create a wrapper environment to save episode returns and episode lengths
         envs_wrapper = gym.wrappers.RecordEpisodeStatistics(envs, deque_size=n_envs * n_updates)
 
-        critic_losses = []
-        actor_losses = []
-        entropies = []
+        # training the agent
+        (critic_losses, actor_losses, entropies) = train.train(agent=agent, envs_wrapper=envs_wrapper, device=device)
+            
+        if is_save_weights:
+            save_load_weights.save_weights(agent)
+
+        if is_load_weights:
+            agent = A2C(num_features, num_actions, device, critic_lr, actor_lr)
+            save_load_weights.load_weights(agent)
         
-        # use tqdm to get a progress bar for training
-        for sample_phase in tqdm(range(n_updates)):
-            # we don't have to reset the envs, they just continue playing
-            # until the episode is over and then reset automatically
-
-            # reset lists that collect experiences of an episode (sample phase)
-            ep_value_preds = torch.zeros(n_steps_per_update, n_envs, device=device)
-            ep_rewards = torch.zeros(n_steps_per_update, n_envs, device=device)
-            ep_action_log_probs = torch.zeros(n_steps_per_update, n_envs, device=device)
-            masks = torch.zeros(n_steps_per_update, n_envs, device=device)
-
-            # at the start of training reset all envs to get an initial state
-            if sample_phase == 0:
-                states, info = envs_wrapper.reset(seed=42)
-
-            # play n steps in our parallel environments to collect data
-            for step in range(n_steps_per_update):
-                
-                states = utility.flatten_observations(states)
-                
-                # select an action A_{t} using S_{t} as input for the agent
-                actions, action_log_probs, state_value_preds, entropy = agent.select_action(
-                    states
-                )
-
-                # perform the action A_{t} in the environment to get S_{t+1} and R_{t+1}
-                states, rewards, terminated, truncated, infos = envs_wrapper.step(
-                    actions.cpu().numpy()
-                )
-
-                ep_value_preds[step] = torch.squeeze(state_value_preds)
-                ep_rewards[step] = torch.tensor(rewards, device=device)
-                ep_action_log_probs[step] = action_log_probs
-
-                # add a mask (for the return calculation later);
-                # for each env the mask is 1 if the episode is ongoing and 0 if it is terminated (not by truncation!)
-                masks[step] = torch.tensor([not term for term in terminated])
-
-                # calculate the losses for actor and critic
-                critic_loss, actor_loss = agent.get_losses(
-                    ep_rewards,
-                    ep_action_log_probs,
-                    ep_value_preds,
-                    entropy,
-                    masks,
-                    gamma,
-                    lam,
-                    ent_coef,
-                    device,
-                )
-
-                # update the actor and critic networks
-                agent.update_parameters(critic_loss, actor_loss)
-
-                # log the losses and entropy
-                critic_losses.append(critic_loss.detach().cpu().numpy())
-                actor_losses.append(actor_loss.detach().cpu().numpy())
-                entropies.append(entropy.detach().mean().cpu().numpy())
-                
-                print("hi")
+        plot_results.plotting(agent, envs_wrapper, critic_losses, actor_losses, entropies)
+        
 
 if __name__ == '__main__':  
     main()
